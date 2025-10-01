8b4ec98bee5707d7a9590e721bacddae3343d754
Emerson Silva <emersondevfullstack1@gmail.com>
2025-10-01 12:36:28 -0300

    feat: creates Hero component UI

--- my portfolio/src/App.tsx
+++ my portfolio/src/App.tsx
@@ -1,7 +1,13 @@
-import "./App.css";
+import Home from "./pages/Home/Home";
+import theme from "./theme";
+import { ThemeProvider } from "@mui/material";
 
 const App = () => {
-  return <>App</>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
 };
 
 export default App;
--- my portfolio/src/main.tsx
+++ my portfolio/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import theme from "./Theme.ts";
+import theme from "./theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
